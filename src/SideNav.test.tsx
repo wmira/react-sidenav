0948375964a2f143b6d25b61f84be4e8f484d835
Warren <warrenmira@gmail.com>
2018-09-17 21:19:33 +0800

    added support for defaultSelectedPath

---
+++ src/SideNav.test.tsx
@@ -0,0 +1,25 @@
+// TODO: seems enzyme is broken!
+// import * as React from 'react'
+// import { shallow } from 'enzyme';
+
+// import { SideNav } from './SideNav'
+// import { Nav } from 'react-sidenav/nav/Nav';
+// import 'jest-enzyme'
+// describe('<SideNav/>', () => {
+//     it('manages state internally if defaultSelectionPath is passed', () => {
+//         const wrapper = shallow((
+//             <SideNav defaultSelectedPath='1'>
+//                 <Nav id="1" />
+//             </SideNav>
+//         ))
+
+//         wrapper.find("Nav").simulate("click")
+
+//     })
+// })
+
+describe('<SideNav/>', () => {
+    it('works', () => {
+        // console.log('for now')
+    })
+})
