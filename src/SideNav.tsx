600ba9375d5e04d6c777014ad4b45d11a137a949
Warren Mira <warrenmira@gmail.com>
2018-09-17 21:23:11 +0800

    added support for defaultSelectedPath (#52)

--- src/SideNav.tsx
+++ src/SideNav.tsx
@@ -11,15 +11,23 @@ const Container = styled.div`
     height: 100%;
 `
 
-export class SideNav extends React.Component<ISideNavProp> {
+interface ISideNavState {
+    selectedPath: string | undefined
+}
 
+export class SideNav extends React.Component<ISideNavProp, ISideNavState> {
+
+    public state: ISideNavState = { selectedPath: undefined }
 
     constructor(props: ISideNavProp) {
         super(props)
+        if ( this.props.defaultSelectedPath ) {
+            this.setState({ selectedPath: this.props.defaultSelectedPath })
+        }
     }
 
     public render() {
-        const { selectedPath } = this.props
+        const selectedPath = this.props.selectedPath || this.props.defaultSelectedPath
         const propsTemplate = { ...this.props.template }
         const template = { ...defaultTemplate, ...propsTemplate }
         const theme = { ...defaultTheme, ...this.props.theme }
@@ -39,6 +47,16 @@ export class SideNav extends React.Component<ISideNavProp> {
 
     private onItemSelection = (arg: IOnItemSelectionArg) => {
 
+        if ( this.state.selectedPath ) {
+            this.setState({ selectedPath: arg.path }, () => {
+                this.dispatchItemSelection(arg)
+            })
+        } else {
+            this.dispatchItemSelection(arg)
+        }
+    }
+
+    private dispatchItemSelection = (arg: IOnItemSelectionArg) => {
         if ( this.props.onItemSelection ) {
             this.props.onItemSelection(arg)
         }
