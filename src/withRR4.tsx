607c2019fadbb4e3c3ee299b9df335f019565d22
Warren <warrenmira@gmail.com>
2018-10-06 07:42:42 +0800

    fixed createLocation and moved things to peerdep

--- src/withRR4.tsx
+++ src/withRR4.tsx
@@ -4,6 +4,7 @@ import { SideNav } from './SideNav'
 
 import { ISideNavProp, INavSelectionArg } from "react-sidenav/types";
 import { withRouter, RouteComponentProps } from 'react-router-dom'
+import { createLocation } from 'history'
 import { PATH_SEPARATOR } from './constants'
 
 type SideNavWithRR4Prop = ISideNavProp & RouteComponentProps<{}> & { onHistoryChange?: (arg: INavSelectionArg) => void }
@@ -31,22 +32,23 @@ export const withRR4 = () => {
         }
 
         public onItemSelection = (arg: INavSelectionArg) => {
-            const { match } = this.props
+            const { match, location } = this.props
             const { payload } = arg
             const withToPayload = payload as { to: string }
-            const slash = match.url.endsWith('/') ? '' : '/'
-            if ( payload && withToPayload.to ) {
-                this.props.history.push(`${match.url}${slash}${withToPayload.to}`)
-            } else {
-                const path = arg.path.split(PATH_SEPARATOR).join('/')
-                this.props.history.push(`${match.url}${slash}${path}`)
 
-            }
             if ( this.props.onItemSelection ) {
                 this.props.onItemSelection(arg)
             }
             if ( this.props.onHistoryChange ) {
                 this.props.onHistoryChange(arg)
+            }
+            if ( payload && withToPayload.to ) {
+                this.props.history.push(createLocation(withToPayload.to, null, null, location))
+
+            } else {
+                const path = arg.path.split(PATH_SEPARATOR).join('/')
+                this.props.history.push(createLocation(path, null, null, location))
+
             }
         }
 
